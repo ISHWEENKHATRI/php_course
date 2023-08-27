5abd224c3383cc151a34776a2f007f92d10c9895
ISHWEENKHATRI <ishween.khatri.csai.20@ggits.net>
2023-08-27 15:15:14 +0530

    pr6

--- 1_syntax.php
+++ 1_syntax.php
@@ -1,1 +1,8 @@
-<?php echo "Hello Ishween"; ?>
+<!-- Basic PHP Syntax
+A PHP script can be placed anywhere in the document.
+
+A PHP script starts with <?php and ends with ?>: -->
+
+<?php
+// PHP code goes here
+?>
